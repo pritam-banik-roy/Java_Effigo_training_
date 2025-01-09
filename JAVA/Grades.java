b14a7d0d1f3220bc9e56ab0a2249fb2e25243157
PRITAM BANIK ROY <127240592+pritam-banik-roy@users.noreply.github.com>
2025-01-09 20:56:10 +0530

    Practice codes

---
+++ JAVA/Grades.java
@@ -0,0 +1,11 @@
+public class Grades {
+    public static void main(String[] args) {
+        char potions='A';
+        char maths='B';
+        char charms='C';
+
+        System.out.println("Potions "+potions);
+        System.out.println("maths "+maths);
+        System.out.println("charms "+charms);
+    }
+}
---
+++ JAVA/decimals.java
@@ -0,0 +1,10 @@
+public class decimals {
+    public static void main(String[] args) {
+        double percentage=73.5;
+        System.out.println(percentage+ "% are made up");
+
+        double wallet=20;
+        int people=3;
+        System.out.println(wallet/people);
+    }
+}
